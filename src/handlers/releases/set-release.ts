4c4bb9876a019b34833157adfe457147e6078631
David Klein <davidk@ravtech.co.il>
2025-01-08 15:31:39 +0200

    Enhance release handling by adding metadata validation and error handling for simultaneous options

--- src/handlers/releases/set-release.ts
+++ src/handlers/releases/set-release.ts
@@ -5,24 +5,44 @@ import { ProjToken } from "../token.handler";
 
 export const handleSetRelease = async (version: string, options?: ReleaseSetOptions) => {
 
+  if (options?.metadata && options?.metadataFile) {
+    console.error('Error: Cannot use both -m and -M options simultaneously.');
+    process.exit(1);
+  }
+
   const projToken = await ProjToken.getTokenOrExit(options?.token) ?? ""
 
   const { projectId } = ProjToken.extractProjId(projToken ?? "")
 
-  let metadate: Record<string, any> | undefined = undefined
+  let metadata: Record<string, any> | undefined;
+
+  const parseMetadata = (data: string) => {
+    try {
+      return JSON.parse(data);
+    } catch (err: any) {
+      console.error(`Invalid metadata, Err: ${err.toString()}`);
+      process.exit(1);
+    }
+  };
+
   if (options?.metadata) {
-    metadate = JSON.parse(options.metadata)
+    metadata = parseMetadata(options.metadata);
   } else if (options?.metadataFile) {
-    metadate = readFromFile(options.metadataFile)
+    const fileContent = await readFromFile(options.metadataFile).catch((err: any) => {
+      console.error(`Failed to read metadata from the given file path - ${options.metadataFile}, Err: ${err.toString()}`);
+      process.exit(1);
+    });
+    metadata = parseMetadata(fileContent);
   }
 
   const data = {
     version,
     name: options?.name,
     releaseNotes: options?.notes,
-    metadate,
+    metadata,
   }
 
   await sendRelSet(data, projToken, projectId)
+  console.log(`Release ${version} set successfully`)
 
 }
